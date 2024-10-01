7592d5f834eb53034945e7036bfc97b3cefc6385
mmrosatab <mmrosadev@gmail.com>
2024-09-30 21:31:53 -0300

    config: configure path alias

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import { fileURLToPath } from 'url'
+
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
       '@/components': path.resolve(__dirname, 'src/components'),
+      '@/views': path.resolve(__dirname, 'src/views'),
       '@/assets': path.resolve(__dirname, 'src/assets'),
       '@/utils': path.resolve(__dirname, 'src/utils'),
+      '@/routes': path.resolve(__dirname, 'src/routes'),
     },
   },
 })
