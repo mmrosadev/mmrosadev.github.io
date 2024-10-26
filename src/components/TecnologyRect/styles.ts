045b22c9393c5090a9b27ef96205ee611c81eb85
mmrosatab <mmrosadev@gmail.com>
2024-10-26 13:11:58 -0300

    style: add translation animation on rect

--- src/components/TecnologyRect/styles.ts
+++ src/components/TecnologyRect/styles.ts
@@ -4,6 +4,23 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+
+    @keyframes shake {
+        0%, 100% {
+            transform: translateX(0); /* Posição inicial e final sem deslocamento */
+        }
+        10%, 30%, 50%, 70%, 90% {
+            transform: translateX(-5px); /* Deslocamento para a esquerda */
+        }
+        20%, 40%, 60%, 80% {
+            transform: translateX(5px); /* Deslocamento para a direita */
+        }
+    }
+
+    div:hover{
+        animation: shake 0.5s;
+    }
+    
 `
 
 export const ContainerIcon = styled.div`
